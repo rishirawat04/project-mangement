c60b7744e33b7eab0db06d83ac94f1c16e8a4989
rishirawat-tech <rawatrishi390@gmail.com>
2025-02-26 10:39:24 +0530

    Initial commit

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react'
+
+type Props = {
+    name:string;
+    buttonComponent?:ReactNode;
+    isSmallText?:boolean
+}
+
+const Header = ({name, buttonComponent, isSmallText = false}: Props) => {
+  return (
+    <div className='mb-5 flex items-center justify-between'>
+        <h1 className={`${isSmallText ? "text-lg" : "text-2xl"} font-semibold dark:text-white`}>{name}</h1>
+        {buttonComponent}
+    </div>
+  )
+}
+
+export default Header
---
+++ server/src/controllers/projectController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient();
+
+
+export const getProjects = async(req:Request, res:Response):Promise<void> => {
+    try {
+        const project = await prisma.project.findMany();
+        res.json(project)
+    } catch (error: any) {
+        res
+          .status(500)
+          .json({ message: `Error retrieving projects: ${error.message}` });
+      }
+}
+
+
+export const createProjects = async(req:Request, res:Response):Promise<void> => {
+    const { name, description, startDate, endDate} = req.body;
+    try {
+        const newProject = await prisma.project.create({
+            data:{
+                name,
+                description,
+                startDate,
+                endDate,
+            }
+        })
+        res.status(201).json(newProject);
+    } catch (error: any) {
+        res
+        .status(500)
+        .json({ message: `Error creating a project: ${error.message}` });
+    }
+}
